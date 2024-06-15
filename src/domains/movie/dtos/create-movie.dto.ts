d9a9a9a9eb54fb9f0c92032473e79e22d12ae33a
João Carlos de Farias Almeida <joaocdfariass@gmail.com>
2024-06-14 21:47:24 -0300

    refactor: adds new fields to dto

--- src/domains/movie/dtos/create-movie.dto.ts
+++ src/domains/movie/dtos/create-movie.dto.ts
@@ -5,6 +5,22 @@ export class CreateMovieDto {
   @IsNotEmpty()
   readonly title: string;
 
+  @IsString()
+  @IsNotEmpty()
+  readonly directorName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly parentalRating: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly duration: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly synopsis: string;
+
   @IsUrl()
   @IsNotEmpty()
   readonly posterUrl: string;
